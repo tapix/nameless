1c3d8a1897968180899b2c9321fa11c1ca3e134f
Erja Nikunen <Erja.Nikunen@metropolia.fi>
2014-09-07 20:24:39 +0300

    korjattu identifiointitietoja

---
+++ Java7Features/src/main/java/fi/metropolia/java7features/StringSwitch.java
@@ -0,0 +1,32 @@
+package fi.metropolia.java7features;
+
+public class StringSwitch {
+
+	private String day;
+
+	public StringSwitch(String day) {
+		this.day = day;
+	}
+
+	public int getDayNumber() {
+		switch (day) {
+		case "monday":
+			return 0;
+		case "tuesday":
+			return 1;
+		case "wednesday":
+			return 2;
+		case "thursday":
+			return 3;
+		case "friday":
+			return 4;
+		case "saturday":
+			return 5;
+		case "sunday":
+			return 6;
+		default:
+			return 0;
+		}
+	}
+
+}
