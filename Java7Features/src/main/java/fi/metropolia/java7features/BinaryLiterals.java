a09d7fc8174a9699071e596e3891f3050cf2ab88
lauriru <lauriru@edunix.metropolia.fi>
2014-09-15 11:03:07 +0300

    Uus feature BinaryLiterals.javaan

--- Java7Features/src/main/java/fi/metropolia/java7features/BinaryLiterals.java
+++ Java7Features/src/main/java/fi/metropolia/java7features/BinaryLiterals.java
@@ -4,7 +4,7 @@ public class BinaryLiterals {
 
 	private int binaryEight = 0b1000;
 	private int binaryHundred = 0b1100100;
-
+        private int binaryLeet = 0b10100111001;
 	public int getBinaryEight() {
 		return binaryEight;
 	}
@@ -13,6 +13,10 @@ public class BinaryLiterals {
 		system.out.print("teht3");
 		return binaryHundred;
 	}
+
+        public int getBinaryLeet() {
+	        return binaryLeet();
+        }
 	
 	public void Test() {
 		System.out.println("Testi");
